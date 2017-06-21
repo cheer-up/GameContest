fe14eb7ba9f64d8f78bd80f40a89035db4b5707a
cheer-up <zhakovnikita@gmail.com>
2017-06-21 13:55:37 +0400

    ui

---
+++ CowsBulls/js/main.js
@@ -0,0 +1,16 @@
+var UIContoller = (function () {
+    var DomString = {
+        input: ".answer-form__input",
+        resultContainer: ".result",
+        resultElement: ".result__el",
+    }
+    return {
+         getDOMstrings: function() {
+            return DomString;
+        }
+    }
+
+})();
+var controller = (function (UI) {
+
+})(UIContoller);
